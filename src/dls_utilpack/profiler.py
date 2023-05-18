cc1bf0d687c2df1346ec572d256d04b41d7becb3
David Erb <david.erb@diamond.ac.uk>
2023-05-18 05:58:08 +0000

    improves profile

--- src/dls_utilpack/profiler.py
+++ src/dls_utilpack/profiler.py
@@ -1,20 +1,21 @@
 import logging
 import threading
 import time
-from typing import Optional
+from typing import Dict, Optional
 
 logger = logging.getLogger(__name__)
 
 
 class Context:
     """
-    Class that holds the total execution time and call count of potentially multiple executions of the same label.
-    Reports its contents as a single-line string.
+    Class that holds the start time of an execution block.
+    Meant to be used in a "with" statement.
+    Reports its results to the given profiler at the exit.
 
-    TODO: Make profiles nestable by accumulating them at the context close.
+    This class is not publically accessible.
     """
 
-    def __init__(self, label: str, profiler: Profiler):
+    def __init__(self, label: str, profiler):
         self.__profiler = profiler
 
         self.__label = label
@@ -32,25 +33,24 @@ class Context:
 
 class Profile:
     """
-    Class that holds the total execution time and call count of potentially multiple executions of the same label.
+    Class that holds the total execution time and call count
+    of potentially multiple executions of the same label.
     Reports its contents as a single-line string.
-
-    TODO: Make profiles nestable by accumulating them at the context close.
     """
 
     def __init__(self, label):
         self.__label = label
-        self.__seconds = 0.0
-        self.__count = 0
+        self.seconds = 0.0
+        self.count = 0
 
     def __str__(self):
-        if self.__count == 0:
+        if self.count == 0:
             average = 0.0
         else:
-            average = self.__seconds / self.__count
+            average = self.seconds / self.count
 
         return (
-            f"{self.__label} called {self.__count} times"
+            f"{self.__label} called {self.count} times"
             f" for average of {'%0.3f' % average} seconds"
         )
 
@@ -62,20 +62,36 @@ class Profiler:
     """
 
     def __init__(self):
-        self.__profiles = {}
+        self.__profiles: Dict[str, Profile] = {}
         self.__lock = threading.RLock()
 
-    def profile(self, label: str) -> Context:
+    def context(self, label: str) -> Context:
         """
-        Return a context to hold the profile timing.
+        Return a context to hold the profile timing for a code block.
 
         Args:
             label (str): label identifying the profile
 
         Returns:
-            Profile: a new profile object, or previously existing one
+            Context: a new context object
         """
         return Context(label, self)
+
+    def profile(self, label: str) -> Profile:
+        """
+        Make the accumulation profile for the given label.
+        Uses previously existing profile, if any, or makes a new instance.
+
+        Args:
+            label (str): label identifying the profile
+        """
+        with self.__lock:
+            profile = self.__profiles.get(label)
+            if profile is None:
+                profile = Profile(label)
+                self.__profiles[label] = profile
+
+        return profile
 
     def accumulate(self, label: str, seconds: float) -> None:
         """
@@ -84,16 +100,9 @@ class Profiler:
 
         Args:
             label (str): label identifying the profile
-
-        Returns:
-            Profile: a new profile object, or previously existing one
+            seconds (float): the number of seconds to accumulate
         """
-        with self.__lock:
-            profile = self.__profiles.get(label)
-            if profile is None:
-                profile = Profile(label)
-                self.__profiles[label] = profile
-
+        profile = self.profile(label)
         profile.seconds += seconds
         profile.count += 1
 
